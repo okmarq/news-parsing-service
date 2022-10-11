31dc967972cb7d743116025c6b24558c217217e6
Joel Okoromi <okmarq@gmail.com>
2022-10-11 14:32:17 +0100

    login done

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -17,4 +17,34 @@ class AuthController extends AbstractController
             'controller_name' => 'AuthController',
         ]);
     }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin(SessionInterface $session): Response
+    {
+        $session->set('role', 'Admin');
+
+        return $this->redirectToRoute('app_news_index');
+    }
+
+    /**
+     * @Route("/moderator", name="moderator")
+     */
+    public function moderator(SessionInterface $session): Response
+    {
+        $session->set('role', 'Moderator');
+
+        return $this->redirectToRoute('app_news_index');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(SessionInterface $session): Response
+    {
+        $session->set('role', null);
+
+        return $this->redirectToRoute('app_auth');
+    }
 }
