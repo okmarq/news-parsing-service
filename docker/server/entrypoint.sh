49adf5bf2fae720df8af8542d4390aaa83ebf142
Joel Okoromi <okmarq@gmail.com>
2022-10-17 07:27:48 +0100

    dockerize service

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+chmod -R ugo+rw /var/www/html/var/log
+
+chmod -R ugo+rw /var/www/html/var/cache
+
+chown -R www-data /var/www/html/var/log/
+
+chown -R www-data /var/www/html/var/cache/
+
 symfony check:requirements
 
 composer install -n
@@ -8,7 +16,7 @@ composer install -n
 # php bin/console make:migration -n
 # php bin/console doctrine:migrations:migrate -n
 
-symfony console doctrine:migrations:migrate -n
+# symfony console doctrine:migrations:migrate -n
 
 # bin/console doc:mig:mig -n
 # bin/console doc:fix:load -n
